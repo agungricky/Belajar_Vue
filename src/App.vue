be24136e217ec3869bff9b890d70304d12f5f08a
agungricky07.github.io <agungricky07@gmail.com>
2024-09-25 15:51:17 +0700

    25/9/2024

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,164 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from "vue";
+const showform = ref(false);
+const memo = ref("");
+const memos = ref([]);
+const error = ref("");
 
-    const count =  ref(0);
+function addMemo() {
+  if (!memo.value) {
+    error.value = "Mohon Isi Form terlebih dahulu";
+    return;
+  }
 
-    function tambah() {
-      count.value++
-    }
+  memos.value.push({
+    id: Date.now(),
+    memo: memo.value,
+    date: new Date().toLocaleDateString("en-GB"),
+    bg: `#${Math.floor(Math.random() * 16777215).toString(16)}`,
+  });
 
-    function kurang() {
-      count.value--
-    }
+  memo.value = "";
+  showform.value = false;
+}
+
+function deletecard(id) {
+  memos.value = memos.value.filter((memo)=>memo.id !== id)
+}
 </script>
 
 <template>
   <main>
-    <div>
-      <h1>Counter</h1>
-      <span id="value">{{ count }}</span>
-      <div>
-        <button @click="tambah">+</button>
-        <button @click="kurang">-</button>
+    <div class="container">
+      <header>
+        <h1 class="title">memo</h1>
+        <button class="button" @click="showform = true">+</button>
+      </header>
+      <div class="card-container">
+        <div v-for="(memo, index) in memos" :key="index" class="card" :style="{ backgroundColor: memo.bg }">
+          <p>{{ memo.memo }}</p>
+          <div class="card-footer">
+            <p class="card-date">{{ memo.date }}</p>
+            <button @click="deletecard(memo.id)">x</button>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <div v-if="showform" class="form-overlay">
+      <div class="form-modal">
+        <button class="close" @click="showform = false">&times;</button>
+        <p v-if="!memo" class="error">{{ error }}</p>
+        <textarea v-model="memo" name="memo" id="memo" cols="30" rows="10"></textarea>
+        <button @click="addMemo" class="save">Simpan</button>
       </div>
     </div>
   </main>
 </template>
 
-<!-- Scoped : digunakan agar css ini hanya diload untuk tampilan ini bukan global -->
-<style scoped>
-
-main{
-    text-align: center;
-    background-color: #333333;
-    padding: 20px;
-    border-radius: 8px;
+<style scope>
+.container {
+  max-width: 90%;
+  padding: 10px;
+  margin: 0 auto;
 }
 
-#value{
-    font-size: 50px;
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
-button{
-    font-size: 16px;
-    margin: 0 5px;
-    background-color: #555555;
-    color: white;
-    border: none;
-    padding: 10px 20px;
-    cursor: pointer;
-    border-radius: 4px;
+.title {
+  font-size: 48px;
+  font-weight: bold;
+  margin-bottom: 25px;
+  color: #495a7d;
+}
+
+.button {
+  border: none;
+  padding: 10px;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  border-radius: 100%;
+  background-color: #495a7d;
+  color: white;
+}
+
+.card-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+}
+
+.card {
+  width: 225px;
+  height: 225px;
+  padding: 10px;
+  background-color: #ffa6c1;
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.form-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.077);
+  z-index: 10;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.form-modal {
+  width: 420px;
+  background-color: white;
+  border-radius: 10px;
+  padding: 30px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+}
+
+.save {
+  margin-top: 15px;
+  padding: 10px 20px;
+  font-size: 20px;
+  width: 100%;
+  background-color: #495a7d;
+  border: none;
+  cursor: pointer;
+  border-radius: 15px;
+  color: white;
+}
+
+.close {
+  position: absolute;
+  top: 5px;
+  right: 10px;
+  width: 30px;
+  height: 30px;
+  background-color: transparent;
+  border: none;
+  font-size: 25px;
+  cursor: pointer;
+}
+
+.error {
+  color: red;
+}
+
+.card-footer{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
